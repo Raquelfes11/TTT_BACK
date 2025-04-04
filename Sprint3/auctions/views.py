a68174febec17f97c39e02fc4bff4f16c0f067b4
Raquelfes11 <202209790@alu.comillas.edu>
2025-04-04 23:16:42 +0200

    CLASE

---
+++ Sprint3/auctions/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from .models import Category, Auction
+from .serializers import AuctionDetailSerializer, AuctionListCreateSerializer, CategoryDetailSerializer, CategoryListCreateSerializer
+
+# Create your views here.
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryListCreateSerializer
+
+class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryDetailSerializer
+
+class AuctionListCreate(generics.ListCreateAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionListCreateSerializer
+
+class AuctionRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionDetailSerializer
+
+
